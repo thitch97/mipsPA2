2ebe889c3bfca0b8dc5d71d01f401185fae403d0
Tim <th.einstein97@gmail.com>
2017-12-01 22:57:47 -0500

    Add input and main function

--- pa2.asm
+++ pa2.asm
@@ -1,6 +1,10 @@
 #-------------------------------------------------------------------------------------
 # Timothy Hitchener
+# Due: December 1, 2017
+# 
+# Registers:
 #
+#	
 #
 #
 #
@@ -9,55 +13,146 @@
 #-------------------------------------------------------------------------------------
 
 .data
+	inval_err: .asciiz "NaN" 		#output for invalid hex 
+	size_err: .asciiz "too large"	
+	newline: .asciiz "\n"			#stores new line for output
+	input: .space 1001				#allocated space for characters
 
 .text
-
 main:
+	jal input_data
+	
+loop:
+	
+	jal find_start_end
+    
+    add $s0, $zero, $v0               
+    add $s1, $zero, $v1                
+    
+   
+    add $a0, $zero, $v0                
+    add $a1, $zero, $v1                
+    
+    jal validity_check
+    
+    
+    add $s2, $zero, $v0                 
+    add $s3, $zero, $v1                 
+    
+   
+    add $a0, $zero, $s0                 
+    add $a1, $zero, $s1                 
+    add $a2, $zero, $s3                
+    add $a3, $zero, $s2                
+    jal subprogram2
+
+    add $a0, $zero, $s2                 
+    jal subprogram3                    
+    
+    addi $a0, $s1, 1                    
+    j loop
+
+end:
+	li $v0, 10
+	syscall
+
+
+input_data:
+    la $a0, input                
+    li $v0, 4
+    syscall           
+                      
+    la $a0, input                                                                                 
+    li $a1, 1000                                                                
+    li $v0, 8                                                                 
+    syscall                             
+    
+    jr $ra
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 		
-subprogram1:
-	
-lower_upper_bound:
-	blt $a0, 103, lower_lower_bound
-	j fail
 
-lower_lower_bound:
-	bgt $a0, 96, store_low_val 
-	j upper_upper_bound
 
-store_low_val:
-	addi $v0, $a0, -87
-	jr $ra
-	
-upper_upper_bound:
-	blt $a0, 71, upper_lower_bound
-	j fail
 
-upper_lower_bound:
-	bgt $a0, 64, store_up_val
-	j num_upper_bound
-
-store_up_val:
-	addi $v0, $a0, -55
-	jr $ra
-
-num_upper_bound:
-	blt $a0, 58, num_lower_bound
-	j fail
-	
-num_lower_bound:
-	bgt $a0, 47, store_num_val
-	j fail
-
-store_num_val: 
-	addi $v0, $a0, -48
-	jr $ra
 
 	
-
-subprogram2:
-
-
-
-
-
-subprogram3:
